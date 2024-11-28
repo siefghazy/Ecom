5c9951a7e163f6d64141fe5b4009cfc70fa3530e
siefghazy <siefghazy5@gmail.com>
2024-11-28 16:16:17 +0200

    add dto

---
+++ Store.Services/DTO/BrandDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Store.Services.DTO
+{
+    public  class BrandDto
+    {
+        public int? brandDtoId { get; set; }
+        public string? Name { get; set; }
+        public List<dynamic> products { get; set; }
+        public string? imageUrl { get; set; }
+        [JsonIgnore]
+        public IFormFile? formImage { get; set; }
+    }
+}
